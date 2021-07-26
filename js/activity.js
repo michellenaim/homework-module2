ef915f249b8ddea84628982f7bf048b688dd6baa
Michelle Naim <michellenaim@Michelles-MacBook-Pro.local>
2021-07-25 20:19:57 -0700

    Add HTML and CSS text for activity selector

--- js/activity.js
+++ js/activity.js
@@ -4,6 +4,23 @@ $(document).ready(function() {
       })
 
         $('td:not(.title)').click(function() {
-            $(this).toggleClass('green')
+            var content = $(this).text
+
+            if (content != "Not Available") {
+              $(this).toggleClass('green')
+
+              if ($(this).hasClass("green")) {
+                $('#displaySelected').css('visibility', 'visible');
+                $('#displaySelected').css('margin-top', '2em');
+                $('#result').append("<p>" + content + "</p>");
+              } else {
+                
+              }
+            }
         })
+
+
+
+
+    
 })      
