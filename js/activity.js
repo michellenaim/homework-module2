1dc16c0519e425e4008db726993aef8e4e77037e
Michelle Naim <michellenaim@Michelles-MacBook-Pro.local>
2021-07-27 18:08:22 -0700

    Add cliff site names using jQuery

--- js/activity.js
+++ js/activity.js
@@ -5,13 +5,14 @@ $(document).ready(function() {
 
         $('td:not(.title)').click(function() {
             var content = $(this).text();
+            var colName = $(this).closest('table').find('th').eq($(this).index()).text();
             if (content != "Not Available") {
               $(this).toggleClass('green')
 
               if ($(this).hasClass("green")) {
                 $('#displaySelected').css('visibility', 'visible');
                 $('#displaySelected').css('margin-top', '2em');
-                $('#result').append("<p>" + content + "</p>");
+                $('#result').append("<p>" + content + " at " + colName + "</p>");
               } else {
                 $('#result p:contains('+content+')').remove();
 
